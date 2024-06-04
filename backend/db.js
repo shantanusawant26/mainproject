076ffb225663aff201a7f0cf48143d012be7d615
shantanusawant26 <shantanusawant26@gmail.com>
2024-06-04 12:57:37 +0530

    changes in db path

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-// Replace with your MongoDB connection string
-const uri = "mongodb://127.0.0.1:27017/vsTechHorizon";
+// // Replace with your MongoDB connection string
+// const uri = "mongodb://127.0.0.1:27017/vsTechHorizon";
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB connection established successfully!"))
-    .catch(err => console.log("MongoDB connection failed: ", err));
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log("MongoDB connection established successfully!"))
+//     .catch(err => console.log("MongoDB connection failed: ", err));
 
-module.exports = mongoose;
+// module.exports = mongoose;
 
 
 
@@ -40,14 +40,14 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
-// const mongoose = require('mongoose');
-// //const uri = "mongodb+srv://shantanusawant26:RgQ7INcGijt5L7iM@cluster0.pbuzob9.mongodb.net/vsTechHorizon?retryWrites=true&w=majority";
-// const uri ="mongodb+srv://vstechhorizon:Software@cluster0.kljlv7f.mongodb.net/vsTechHorizon?retryWrites=true&w=majority&appName=Cluster0";
-// // Connect to MongoDB Atlas
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//         console.log('Connected to MongoDB Atlas');
-//         // If connected successfully, proceed to save data
-//         // saveData();
-//     })
-//     .catch(err => console.error('Failed to connect to MongoDB Atlas:', err));
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://shantanusawant26:RgQ7INcGijt5L7iM@cluster0.pbuzob9.mongodb.net/vsTechHorizon?retryWrites=true&w=majority";
+//const uri ="mongodb+srv://vstechhorizon:Software@cluster0.kljlv7f.mongodb.net/vsTechHorizon?retryWrites=true&w=majority&appName=Cluster0";
+// Connect to MongoDB Atlas
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+        // If connected successfully, proceed to save data
+        // saveData();
+    })
+    .catch(err => console.error('Failed to connect to MongoDB Atlas:', err));
